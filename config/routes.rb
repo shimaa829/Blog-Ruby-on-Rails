9e950589cd67f85a826969a3c55aa54565f011bf
shimaa829 <shimaa.abdelazim95@gmail.com>
2020-03-27 10:26:54 +0200

    create bolg comments with forms using bootstrap

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,9 @@
 Rails.application.routes.draw do
-  resources :comments
+ 
   # get 'post/index'
-  get 'index' => 'post#index'
-  root 'post#index'
-  resources :posts
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  resources :posts do
+  	resources :comments
+  end
+  root 'posts#index'
+ 
 end
